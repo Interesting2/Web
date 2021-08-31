15cf66f1129610143022b2a3b9c09845f7499fea
IO <ilei0920@uni.sydney.edu.au>
2021-08-31 20:53:53 +1000

    Modified Side menu and colors

--- action.js
+++ action.js
@@ -1,9 +1,15 @@
 
 let sideMenu = document.getElementById('main-side');
+let viewPort = document.getElementsByTagName('body');
 
 function closeMenu() {
-    sideMenu.style.display = 'none';
+    sideMenu.style.transition = 'width 2.5s';
+    sideMenu.style.width = '0';
+    viewPort[0].style.marginRight = "0";
 }
 function openMenu() {
     sideMenu.style.display = 'block';
+    viewPort[0].style.marginRight = "200px";
+    sideMenu.style.transition = 'width ease-in-out 2.5s';
+    sideMenu.style.width = '500px';
 }
