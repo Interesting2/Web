946d122ec8fe55b569513884f4cfd0817b1957dd
IO <ilei0920@uni.sydney.edu.au>
2021-09-01 22:40:20 +1000

    Add shade

--- action.js
+++ action.js
@@ -1,15 +1,17 @@
 
 let sideMenu = document.getElementById('main-side');
 let viewPort = document.getElementsByTagName('body');
+let navWindow = document.getElementsByClassName('nav-window');
 
 function closeMenu() {
-    sideMenu.style.transition = 'width 2.5s';
+    sideMenu.style.transition = 'width 2s';
     sideMenu.style.width = '0';
     viewPort[0].style.marginRight = "0";
 }
 function openMenu() {
     sideMenu.style.display = 'block';
-    viewPort[0].style.marginRight = "200px";
-    sideMenu.style.transition = 'width ease-in-out 2.5s';
+    //viewPort[0].style.marginRight = "500px";
+    sideMenu.style.transition = 'width ease-in-out 2s';
     sideMenu.style.width = '500px';
+
 }
