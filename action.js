d6a35c22838242a10e922e5d34f2f1a6cafe0d4e
IO <ilei0920@uni.sydney.edu.au>
2021-09-09 21:42:20 +1000

    Refactor nav-bar and side menu

--- action.js
+++ action.js
@@ -8,27 +8,26 @@ let navContainer = document.getElementsByClassName('nav-container');
 function closeMenu() {
     sideMenu.style.transition = 'width 2s';
     sideMenu.style.width = '0';
+    sideMenu.style.display = 'none';
     viewPort[0].style.marginRight = "0";
     viewWindow[0].style.cssText = '';
     navContainer[0].style.cssText -= 'z-index: -1';
 }
 function openMenu() {
     sideMenu.style.display = 'block';
-    //viewPort[0].style.marginRight = "500px";
+    viewPort[0].style.marginRight = "500px";
     sideMenu.style.transition = 'width ease-in-out 2s';
     sideMenu.style.width = '500px';
     let styles = `
         .view-port {
             z-index: -1;
             background-color: rgba(0, 0, 0, 0.2);
-            height: 100%;
-            width: 100%;
+            min-height: 100vh;
+            min-width: 100%;
         }
     `
-    viewWindow[0].style.cssText = 'z-index: 999;background-color: rgba(0,0,0,0.5);height: 100%;width: 100%;';
+    viewWindow[0].style.cssText = 'position:absolute;top:0;left:0;z-index: 100;background-color: rgba(0,0,0,0.5); height: 100%;width: 100%;';
     //console.log(viewWindow[0]);
     navContainer[0].style.cssText += 'z-index: -1';
-
-
 
 }
