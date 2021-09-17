8daebe9fcff9eb9bccbc424d0ab396de90ea3b83
IO <ilei0920@uni.sydney.edu.au>
2021-09-17 19:27:26 +1000

    Done with Side Menu and Nav Bar

--- action.js
+++ action.js
@@ -4,30 +4,71 @@ let viewPort = document.getElementsByTagName('body');
 let navWindow = document.getElementsByClassName('nav-window');
 let viewWindow = document.getElementsByClassName('view-port');
 let navContainer = document.getElementsByClassName('nav-container');
+let subContentContainer = document.getElementById('side-sub-target');
 
 function closeMenu() {
-    sideMenu.style.transition = 'width 2s';
-    sideMenu.style.width = '0';
-    sideMenu.style.display = 'none';
+    sideMenu.style.cssText -= 'transform: translateX(0);';
+    sideMenu.style.cssText += 'transform: translateX(500px);';
     viewPort[0].style.marginRight = "0";
     viewWindow[0].style.cssText = '';
-    navContainer[0].style.cssText -= 'z-index: -1';
+    
+    let subContainer = document.getElementById('side-sub-content-container');
+    let mainContainer = document.getElementById('side-content-container'); 
+    subContainer.style.cssText += 'transform: translateX(-500px);';
+    mainContainer.style.cssText += 'transform: translateX(0)';
+    subContentContainer.innerHTML = "";
+
 }
 function openMenu() {
-    sideMenu.style.display = 'block';
     viewPort[0].style.marginRight = "500px";
-    sideMenu.style.transition = 'width ease-in-out 2s';
-    sideMenu.style.width = '500px';
-    let styles = `
-        .view-port {
-            z-index: -1;
-            background-color: rgba(0, 0, 0, 0.2);
-            min-height: 100vh;
-            min-width: 100%;
-        }
-    `
+    sideMenu.style.cssText -= 'transform: translateX(500px);';
+    sideMenu.style.cssText += 'transform: translateX(0);';
+
     viewWindow[0].style.cssText = 'position:absolute;top:0;left:0;z-index: 100;background-color: rgba(0,0,0,0.5); height: 100%;width: 100%;';
     //console.log(viewWindow[0]);
-    navContainer[0].style.cssText += 'z-index: -1';
+}
 
+// Show side-sub-content-container
+document.querySelectorAll('.side-new-content').forEach(row => {
+        row.addEventListener('click', () => {
+            let subContainer = document.getElementById('side-sub-content-container');
+            let mainContainer = document.getElementById('side-content-container'); 
+            subContainer.style.cssText += 'transform: translateX(0);';
+            mainContainer.style.cssText += 'transform: translateX(500px)';
+        });    
+});
+
+document.getElementById('main-menu').addEventListener('click', () => {
+
+    let subContainer = document.getElementById('side-sub-content-container');
+    let mainContainer = document.getElementById('side-content-container'); 
+    subContainer.style.cssText += 'transform: translateX(-500px);';
+    mainContainer.style.cssText += 'transform: translateX(0)';
+    subContentContainer.innerHTML = "";
+});
+
+
+
+
+function openNewProduct1() {
+    subContentContainer.innerHTML = '<div class="side-content-header">Beauty & Skincare</div>\
+                                    <a href="#"> \
+                                    <div class="side-new-content">\
+                                        <div class="side-link">Cat 1</div>\
+                                        <i class="fa fa-hand-o-right"></i>\
+                                    </div>\
+                                    </a>\
+                                    <a href="#">\
+                                    <div class="side-new-content">\
+                                        <div class="side-link">Cat 2</div>\
+                                        <i class="fa fa-hand-o-right"></i>\
+                                    </div>\
+                                    </a>\
+                                    <a href="#">\
+                                    <div class="side-new-content">\
+                                        <div class="side-link">Cat 3</div>\
+                                        <i class="fa fa-hand-o-right"></i>\
+                                    </div>\
+                                    </a>\
+                                    <hr>'
 }
