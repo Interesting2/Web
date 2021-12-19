9901b7c41d86915819c6856b246f7f3b0036daa6
IO <ilei0920@uni.sydney.edu.au>
2021-12-19 21:06:50 +1100

    config db

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -22,27 +22,32 @@ exports.loadClothings = (req, res, next) => {
     pool.connect()
     .then((client) => {
         // return {'message': 'canget'}
-        return client.query("SELECT * FROM products")
-        //return client.query(`SELECT * FROM ecom_products.products
-        //                         WHERE category = $1
-        //                         AND id >= $2
-        //                         ORDER BY id ASC
-        //                         LIMIT $3`, [req.params.category, id, amount])
-        .then(results => {
-            // client.release();
-            // let result = results.rows;
-            // console.log(result);
-            return {'message': 'canget'};
-        })
-        .then(result => {
-            res.setHeader('Content-Type', 'application/json');
-            res.json(result);
-        })
-        .catch(e => console.log("Error in query: \n" + e))
-        .finally(() => {
-            client.release();
-            console.log("Connection ended");
-        });
+        function getQuery() {
+            return {'message': 'canget'}
+            // return client.query("SELECT * FROM products")
+            //return client.query(`SELECT * FROM ecom_products.products
+            //                         WHERE category = $1
+            //                         AND id >= $2
+            //                         ORDER BY id ASC
+            //                         LIMIT $3`, [req.params.category, id, amount])
+            .then(results => {
+                // client.release();
+                // let result = results.rows;
+                // console.log(result);
+                return results;
+            })
+            .then(result => {
+                res.setHeader('Content-Type', 'application/json');
+                res.json(result);
+            })
+            .catch(e => console.log("Error in query: \n" + e))
+            .finally(() => {
+                client.release();
+                console.log("Connection ended");
+            });
+        }
+        getQuery();
+        return;
     })
     .catch(e => console.log("Error in connection: \n" + e)); 
     // res.render(req.params.page);
