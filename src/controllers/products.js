79cd0e44e47156e474425dbbc2a8ac15c6e4f83f
IO <ilei0920@uni.sydney.edu.au>
2021-12-19 21:49:59 +1100

    inserted data into db

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -46,7 +46,7 @@ exports.loadClothings = (req, res, next) => {
                 console.log("Connection ended");
             });
         }
-        getQuery("SELECT * FROM products limit $1", [amount]);
+        getQuery("SELECT * FROM products limit 3");
         return;
     })
     .catch(e => console.log("Error in connection: \n" + e)); 
