dc37755b55102edeb96516977da705b3609f557c
IO <ilei0920@uni.sydney.edu.au>
2021-12-19 17:52:35 +1100

    test post request

--- src/routes/products.js
+++ src/routes/products.js
@@ -6,6 +6,6 @@ const productController = require('../controllers/products');
 const router = express.Router();
 // const staticPath = path.join(__dirname, '..', 'static_content');
 
-router.post('clothings/:category', productController.loadClothings);   // render pages or load more function
+router.post('/clothings/clothing', productController.loadClothings);   // render pages or load more function
 
 module.exports = router;
