ee425426a18b2c1ec1ad7d84755b953800e9d74e
IO <ilei0920@uni.sydney.edu.au>
2021-12-19 20:40:27 +1100

    config db

--- src/util/database.js
+++ src/util/database.js
@@ -10,10 +10,8 @@ const { Pool } = require('pg');
 // });
 
 const pool = new Pool({
-	connectionString: "postgres://bvgrxquskuhact:444c775051ba5a4b5a98886e8d4b8cf7c24d1476c4668e1fcb8e0fcf69d26fd2@ec2-52-86-177-34.compute-1.amazonaws.com:5432/dera0birnk6mur",
-    ssl: {
-        rejectUnauthorized: false
-    }
+	connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
 
 pool.on('error', (err, client) => {
