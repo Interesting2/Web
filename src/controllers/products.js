97aec075ac5820c5efec3a363b5eed7b674c26d8
IO <ilei0920@uni.sydney.edu.au>
2021-12-19 17:58:09 +1100

    test post request

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -17,36 +17,36 @@ exports.loadClothings = (req, res, next) => {
     // console.log("Request value: " + req.body.id);
     console.log("Request param: " + req.params.category);
     // get request if any
-    return res.json({'message': 'cannot'});
+    // return res.json({'message': 'cannot'});
 
-    // pool.connect()
-    // .then((client) => {
-    //     return {'message': 'canget'}
-    //     // return client.query(`SELECT * FROM ecom_products.products
-    //     //                         LIMIT $1`, [amount])
-    //     // return client.query(`SELECT * FROM ecom_products.products
-    //     //                         WHERE category = $1
-    //     //                         AND id >= $2
-    //     //                         ORDER BY id ASC
-    //     //                         LIMIT $3`, [req.params.category, id, amount])
-    //     .then(results => {
-    //         // client.release();
-    //         // let result = results.rows;
-    //         // console.log(result);
-    //         return results;
-    //     })
-    //     .then(result => {
-    //         res.setHeader('Content-Type', 'application/json');
-    //         res.json(result);
-    //     })
-    //     .catch(e => console.log("Error in query: \n" + e))
-    //     .finally(() => {
-    //         client.release();
-    //         console.log("Connection ended");
-    //     });
-    // })
-    // .catch(e => console.log("Error in connection: \n" + e)); 
-    // // res.render(req.params.page);
-    // // res.json(result);
+    pool.connect()
+    .then((client) => {
+        return {'message': 'canget'}
+        // return client.query(`SELECT * FROM ecom_products.products
+        //                         LIMIT $1`, [amount])
+        // return client.query(`SELECT * FROM ecom_products.products
+        //                         WHERE category = $1
+        //                         AND id >= $2
+        //                         ORDER BY id ASC
+        //                         LIMIT $3`, [req.params.category, id, amount])
+        .then(results => {
+            // client.release();
+            // let result = results.rows;
+            // console.log(result);
+            return results;
+        })
+        .then(result => {
+            res.setHeader('Content-Type', 'application/json');
+            res.json(result);
+        })
+        .catch(e => console.log("Error in query: \n" + e))
+        .finally(() => {
+            client.release();
+            console.log("Connection ended");
+        });
+    })
+    .catch(e => console.log("Error in connection: \n" + e)); 
+    // res.render(req.params.page);
+    // res.json(result);
 
 }
