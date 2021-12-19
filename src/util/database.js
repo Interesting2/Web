6f9616f2ff69db0f4aff3674fdd6cc58949d6b0f
IO <ilei0920@uni.sydney.edu.au>
2021-12-19 15:43:48 +1100

    deploy website with heroku

---
+++ src/util/database.js
@@ -0,0 +1,39 @@
+// const { Client } = require('pg');
+const { Pool } = require('pg');
+
+// const client = new Client({
+// 	user: "postgres",
+// 	password: "08051135",
+// 	host: "localhost",
+// 	port: "5432",
+// 	database: "list_of_all_products"
+// });
+
+const pool = new Pool({
+	user: "postgres",
+	password: "08051135",
+	host: "localhost",
+	port: "5432",
+	database: "list_of_all_products",
+	max: 20,
+	connectionTimeoutMillis: 0,
+	idleTimeoutMillis: 0
+});
+
+pool.on('error', (err, client) => {
+	console.error('Error!!!: Unexpected error on idle client', err) // your callback here
+	// process.exit(-1);
+});
+
+// // conenct database
+// client.connect()
+// .then(() => console.log("Connection Success"))
+// .then(() => client.query("SELECT * FROM ecom_products.products"))
+// .then(() => client.query("SELECT * FROM ecom_products.products WHERE category = $1", ['clothing']))
+// .then(results => console.table(results.rows))
+// .catch(e => console.log(e))
+// .finally(() => client.end());
+
+// exports.client = client;
+exports.pool = pool;
+// exports.getDB = getDB;
